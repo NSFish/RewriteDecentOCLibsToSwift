9d651186c3b341a05cef7b0c1393598acae70689
NSFish <lxy910428@gmail.com>
2019-04-13 22:46:57 +0800

    Start rewriting UITableView-FDTemplateLayoutCell

---
+++ UITableView-FDTemplateLayoutCell/UITableView-FDTemplateLayoutCell/UITableView_FDTemplateLayoutCell.h
@@ -0,0 +1,19 @@
+//
+//  UITableView_FDTemplateLayoutCell.h
+//  UITableView-FDTemplateLayoutCell
+//
+//  Created by nsfish on 2019/4/13.
+//  Copyright © 2019 nsfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITableView_FDTemplateLayoutCell.
+FOUNDATION_EXPORT double UITableView_FDTemplateLayoutCellVersionNumber;
+
+//! Project version string for UITableView_FDTemplateLayoutCell.
+FOUNDATION_EXPORT const unsigned char UITableView_FDTemplateLayoutCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITableView_FDTemplateLayoutCell/PublicHeader.h>
+
+
