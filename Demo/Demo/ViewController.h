4cbd480d1b225314101d8e9132fe34f0f9d1e6f1
NSFish <lxy910428@gmail.com>
2019-04-15 21:45:59 +0800

    Add demo to UITableView-FDTemplateLayoutCell

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by nsfish on 2019/4/15.
+//  Copyright © 2019 nsfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableView-FDTemplateLayoutCell/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by nsfish on 2019/4/15.
+//  Copyright © 2019 nsfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
